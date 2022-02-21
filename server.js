d7a76746e0e494cd79d63fd49e7c8d0229f22318
roshanwickramaarachchi <roshan.wickrama@arimaclanka.com@gmail.com>
2022-02-21 20:55:04 +0530

    changed logs

--- server.js
+++ server.js
@@ -77,11 +77,19 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(
+// const server = app.listen(
+//   PORT,
+//   console.log(
+//     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold,
+//   ),
+// );
+
+//production
+const server =app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold,
-  ),
+  function () {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
+  }
 );
 
 // Handle unhandled promise rejections
