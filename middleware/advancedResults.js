b41aef938aa9dc42b4ffe6f766b68d248781f3de
roshanwickramaarachchi <roshan.wickrama@arimaclanka.com@gmail.com>
2022-02-21 20:48:14 +0530

    remove pagination

--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -34,13 +34,13 @@ const advancedResults = (model, populate) => async (req, res, next) => {
   }
 
   // Pagination
-  const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 1;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await model.countDocuments();
+  // const page = parseInt(req.query.page, 10) || 1;
+  // const limit = parseInt(req.query.limit, 10) || 1;
+  // const startIndex = (page - 1) * limit;
+  // const endIndex = page * limit;
+  // const total = await model.countDocuments();
 
-  query = query.skip(startIndex).limit(limit);
+  // query = query.skip(startIndex).limit(limit);
 
   if (populate) {
     query = query.populate(populate);
